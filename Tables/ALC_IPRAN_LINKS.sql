6ca769d7a8ff61d54a5308839613c2bde8d55ec1
Marcelo Carrasco <marceloacarrasco@gmail.com>
2017-01-02 10:24:36 -0300

    varios atrasados

---
+++ Tables/ALC_IPRAN_LINKS.sql
@@ -0,0 +1,69 @@
+-- drop table ALC_IPRAN_LINKS purge;
+
+CREATE TABLE ALC_IPRAN_LINKS (
+  ELEMENT_ID        NUMBER NOT NULL ENABLE, 
+  ELEMENT_ALIASES   VARCHAR2(500 CHAR), 
+  VALID_START_DATE  DATE, 
+  VALID_FINISH_DATE DATE, 
+  TIPO              VARCHAR2(40 CHAR), 
+  ORIGEN            VARCHAR2(40 CHAR), 
+  DESTINO           VARCHAR2(40 CHAR), 
+  FLAG_ENABLED      CHAR(1 CHAR), 
+  GRUPO             VARCHAR2(50 CHAR), 
+  PAIS              VARCHAR2(20 CHAR), 
+  ELEMENT_TYPE      VARCHAR2(20 CHAR), 
+  ELEMENT_NAME      VARCHAR2(20 CHAR), 
+  INTERFACE_NAME    VARCHAR2(500 CHAR), 
+  GROUP_TYPE        VARCHAR2(20 CHAR), 
+  ELEMENT_IP        VARCHAR2(15 CHAR), 
+  NOMBRE_GRUPO      VARCHAR2(50 CHAR), 
+  FRONTERA          VARCHAR2(30 CHAR), 
+  SPEED_MODIFY      NUMBER
+ ) NOCOMPRESS NOLOGGING
+TABLESPACE DEVELOPER ;
+
+
+ALTER TABLE ALC_IPRAN_LINKS ADD CONSTRAINT PK_ALC_IPRAN_LINKS PRIMARY KEY (ELEMENT_ID);
+---------------------------------------------------------------------------------------
+-- SYNONYM
+---------------------------------------------------------------------------------------
+CREATE PUBLIC SYNONYM ALC_IPRAN_LINKS FOR MCARRASCO.ALC_IPRAN_LINKS;
+---------------------------------------------------------------------------------------
+-- PRIV.
+---------------------------------------------------------------------------------------
+GRANT SELECT ON ALC_IPRAN_LINKS TO FRINALDI;
+GRANT SELECT ON ALC_IPRAN_LINKS TO MSTUYCK;
+---------------------------------------------------------------------------------------
+-- TAPI
+---------------------------------------------------------------------------------------
+-- DROP TYPE ALC_IPRAN_LINKS_TAPI_REC;
+
+CREATE type ALC_IPRAN_LINKS_TAPI_REC AS OBJECT  (
+    ELEMENT_ID	      NUMBER,
+    ELEMENT_ALIASES	  VARCHAR2(500 CHAR),
+    VALID_START_DATE	DATE,
+    VALID_FINISH_DATE	DATE,
+    TIPO	            VARCHAR2(40 CHAR),
+    ORIGEN	          VARCHAR2(40 CHAR),
+    DESTINO	          VARCHAR2(40 CHAR),
+    FLAG_ENABLED	    CHAR(1 CHAR),
+    GRUPO	            VARCHAR2(50 CHAR),
+    PAIS	            VARCHAR2(20 CHAR),
+    ELEMENT_TYPE	    VARCHAR2(20 CHAR),
+    ELEMENT_NAME	    VARCHAR2(20 CHAR),
+    INTERFACE_NAME	  VARCHAR2(500 CHAR),
+    GROUP_TYPE	      VARCHAR2(20 CHAR),
+    ELEMENT_IP	      VARCHAR2(15 CHAR),
+    NOMBRE_GRUPO	    VARCHAR2(50 CHAR),
+    FRONTERA	        VARCHAR2(30 CHAR),
+    SPEED_MODIFY	    NUMBER 
+);
+    
+    
+CREATE TYPE ALC_IPRAN_LINKS_tapi_tab IS TABLE OF ALC_IPRAN_LINKS_TAPI_REC;
+
+
+ -- insert
+  PROCEDURE INS_ALC_IPRAN_LINKS(P_DATASET IN ALC_IPRAN_LINKS_TAPI_REC);
+  -- update
+  PROCEDURE UPD_ALC_IPRAN_LINKS(P_DATASET IN ALC_IPRAN_LINKS_TAPI_REC);
---
+++ Tables/PROCESS_TO_RUN.sql
@@ -0,0 +1,29 @@
+--------------------------------------------------------
+--  DDL for Table PROCESS_TO_RUN
+--------------------------------------------------------
+
+  CREATE TABLE PROCESS_TO_RUN 
+   (	PROCESS_NAME VARCHAR2(500 CHAR), 
+	PARAMS VARCHAR2(500 CHAR), 
+	FECHA_TO_RUN VARCHAR2(13 CHAR), 
+	TIPO VARCHAR2(50 CHAR), 
+	GRUPO VARCHAR2(50 CHAR), 
+	STATUS NUMBER(1,0) DEFAULT 1, 
+	PROCESADO DATE, 
+	JOB_NAME VARCHAR2(500 CHAR) GENERATED ALWAYS AS (SUBSTR(PROCESS_NAME,INSTR(PROCESS_NAME,'/',-1)+1,LENGTH(PROCESS_NAME))) VIRTUAL 
+   ) 
+ NOCOMPRESS NOLOGGING
+    TABLESPACE DEVELOPER ;
+
+   COMMENT ON COLUMN PROCESS_TO_RUN.FECHA_TO_RUN IS 'Representa la fecha-hora que se pasara como parametro al Job';
+   COMMENT ON TABLE PROCESS_TO_RUN  IS 'Contiene todos los procesos PENTAHO (.kjb) que se ejecutaran, funciona como una cola';
+
+--------------------------------------------------------
+--  Constraints for Table PROCESS_TO_RUN
+--------------------------------------------------------
+
+  ALTER TABLE PROCESS_TO_RUN ADD CONSTRAINT CHK_PROCESS_TO_RUN CHECK (tipo in ('Hourly','Daily','Weekly','Monthly')) ENABLE;
+  ALTER TABLE PROCESS_TO_RUN MODIFY (STATUS NOT NULL ENABLE);
+  ALTER TABLE PROCESS_TO_RUN MODIFY (FECHA_TO_RUN NOT NULL ENABLE);
+  ALTER TABLE PROCESS_TO_RUN MODIFY (PROCESS_NAME NOT NULL ENABLE);
+  ALTER TABLE PROCESS_TO_RUN ADD CONSTRAINT PK_PROCESSTORUN PRIMARY KEY (PROCESS_NAME, FECHA_TO_RUN) ENABLE;
